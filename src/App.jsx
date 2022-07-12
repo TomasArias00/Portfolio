e42a4dadbd8620f2bd3b673b37a9da5496db9e7f
Tomas Arias <tomasarias2000@gmail.com>
2022-07-12 18:32:53 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from './components/Header/Header.jsx'
+
+function App() {
+
+  return (
+    <Header/>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+function Header() {
+  
+    return (
+      <div className={styles.header}>adgadgadHOLAA</div>
+    )
+  }
+
+
+export default Header;
